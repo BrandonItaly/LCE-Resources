87a685a8ccf7b007d420fbdc5193a9e9e9da963a
Brandon Flaherty <brandon.flaherty@outlook.com>
2025-12-05 21:21:11 -0800

    1.0.10

---
+++ README.md
@@ -0,0 +1,14 @@
+# Fantasy Texture Pack
+
+<table>
+  <thead>
+    <tr>
+      <th align="center">Icon</th>
+      <th>Name</th>
+      <th>Downloads</th>
+    </tr>
+  </thead>
+  <tbody>
+  <tr><td><img src="https://cdn.modrinth.com/data/kFbEIMmd/2d3d64ef4a05a60e422abcb0c6fc5259cea9b1a0.png" width="64" height="64" alt="Fantasy Texture Pack icon" /></td><td style="text-align: center;"><strong><a href="https://modrinth.com/resourcepack/fantasy-texture-pack">Fantasy Texture Pack</a></strong></td><td><img src="https://img.shields.io/modrinth/dt/fantasy-texture-pack?logo=Modrinth&label=Downloads" /></td></tr>
+  </tbody>
+</table>
---
+++ resources/assets/minecraft/shaders/include/color_overrides.glsl
@@ -0,0 +1,39 @@
+#version 150
+
+const vec3 black = vec3(0, 0, 0);
+
+const vec3 dark_blue = vec3(0.35, 0.35, 1);
+
+const vec3 dark_green = vec3(0, 0.667, 0);
+
+const vec3 dark_aqua = vec3(0, 0.667, 0.667);
+
+const vec3 dark_red = vec3(0.667, 0, 0);
+
+const vec3 dark_purple = vec3(1, 0.392, 1);
+
+const vec3 gold = vec3(1, 0.667, 0);
+
+const vec3 gray = vec3(0.722, 0.627, 0.447);
+
+const vec3 dark_gray = vec3(0.807, 0.58, 0.255);
+
+const vec3 blue = vec3(0.8588, 0.8588, 1);
+
+const vec3 green = vec3(0.333, 1, 0.333);
+
+const vec3 aqua = vec3(0.333, 1, 1);
+
+const vec3 red = vec3(1, 0.333, 0.333);
+
+const vec3 light_purple = vec3(1, 0.588, 1);
+
+const vec3 yellow = vec3(1, 1, 0.333);
+
+const vec3 white = vec3(1, 1, 1);
+
+const vec3 title_text_color = vec3(1, 0.94, 0.59);
+
+const vec3 body_text_color = vec3(1, 1, 1);
+
+const vec3 experience_text_color = vec3(0.5, 1, 0);
